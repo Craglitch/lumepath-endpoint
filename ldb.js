dbe05ec344585dce499634572d00adcae240e84d
Craglitch <cariuntung92@gmail.com>
2025-11-15 12:55:42 +0800

    add post api

--- ldb.js
+++ ldb.js
@@ -7,24 +7,28 @@ const authRoutes = require('./routes/auth');
 const habitRoutes = require('./routes/habits');
 const taskRoutes = require('./routes/tasks');
 
+// new route
+const postRoutes = require('./routes/posts');
+
+
+// old route jangan lupa buang 
 // ✅ newly added routes [refer to AMIRUL]
-const postRoutes = require('./routes/post');
-const threadRoutes = require('./routes/thread');
-const groupRoutes = require('./routes/group');
+// const postRoutes = require('./routes/posts');
+// const threadRoutes = require('./routes/thread');
+// const groupRoutes = require('./routes/group');
 
 const app = express();
 
 
 // refer to AMIRUL
-// ✅ Allow requests from any host in dev (keep cookies working)
-app.use(
-  cors({
-    origin: true,           // reflect the request origin
-    credentials: true,      // allow cookies to be sent
-  })
-);
-
-
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://a16cc7aca9fd.ngrok-free.app", // debug url
+    "https://lumepath.vercel.app"
+  ],
+  credentials: true
+}));
 // refer to AMIRUL
 // ✅ Middlewares
 app.use(express.json());
@@ -35,10 +39,11 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/habits', habitRoutes);
 app.use('/api/tasks', taskRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/thread', threadRoutes);
-app.use('/api/group', groupRoutes);
-
+//app.use('/api/post', postRoutes);
+//app.use('/api/thread', threadRoutes);
+//app.use('/api/group', groupRoutes);
+// In your server.js/app.js
+app.use('/api/posts', postRoutes);
 // ✅ MongoDB connection [local]
 mongoose
   .connect('mongodb://localhost:27017/lumepath', {
